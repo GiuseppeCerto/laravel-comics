d7e6008215b031f80cf5a2e8d4f8854348365c9a
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-04-09 19:07:11 +0200

    EDIT foreach fot cycle comics card

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+
+<header class="container">
+    <div class="menu">
+
+        <img class="logo_header" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+    
+        <ul class="menu_header">
+
+
+        </ul>
+    </div>
+</header>
